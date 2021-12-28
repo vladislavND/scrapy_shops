648b673553fc1f9726af3ce8f09b83c09cd61a35
yotamvladgmail.com <yotamvlad@gmail.com>
2021-12-28 20:22:10 +0600

    vkusvill fix

--- parsing/spiders/vkusvill.py
+++ parsing/spiders/vkusvill.py
@@ -15,7 +15,7 @@ class VkusvillSpider(SitemapSpider):
         if response.xpath('//img[@class="lazyload"]/@title').get() is not None:
             name = response.xpath('//img[@class="lazyload"]/@title').get()
             image_url = response.xpath('//img[@class="lazyload"]/@data-src').get()
-            price = response.xpath('//span[@class="Price__value"]/text()').get()
+            price = response.xpath('//meta[@itemprop=\"price\"]/@content').get()
             ves_and_year = response.xpath('//li[@class="Product__listItem"]/text()').getall()
             try:
                 ed_izm = response.xpath('//span[@class="Price__unit"]/text()').getall()[1]
@@ -28,7 +28,7 @@ class VkusvillSpider(SitemapSpider):
             product = {
                 'name': name,
                 'unit': ed_izm.replace(' ', '').replace('/', '').replace('\xa0\xa0', ''),
-                'price': unicodedata.normalize('NFKD', price).replace(' ', ''),
+                'price': price,
                 'weight': ves,
                 'image_url': image_url,
                 'url': response.url,
